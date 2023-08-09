eb87eaeeb76ab169ccce4f6de456dc3225e17822
AlanLab <alan.tan.tian@outlook.com>
2023-08-09 14:32:43 -0700

    prepared for first draft

--- README.md
+++ README.md
@@ -12,17 +12,17 @@ Full-length preprint is coming soon.
 
 ## How to Use
 ### Environment
+Our code is developed under the following environment. Versions different from ours may still work.
+
 Python 3.8; Pytorch 1.7.0; Cuda 11.0; Cudnn 8.0.4; matplotlib 3.3.2; numpy 1.19.4; h5py 3.0.0; Scikit-learn 0.23.2
 
-Versions different from ours may still work.
+### Example code
+[An example script](./example_usage/run_example.py)
+that loads one [example dataset](./example_usage/Camargo_levelground.h5) [3],
+fine-tunes the pre-trained [transformer model](./example_usage/pretrained_model_weights.pth)., and
+evaluates the fine-tuned model on the test set.
 
-## Pre-trained Tranformers
-
-[//]: # (&#40;1&#41; [a fusion model based on eight IMUs and cameras]&#40;./trained_models_and_example_data/8IMU_camera.pth&#41;)
-
-[//]: # (### Example code)
-
-#### IMUs
+### IMUs
 One to eight IMUs should be placed on the body as shown in the figure below.
 When using less than eight IMUs, specify the IMU indices in the `--imu_idx` argument.
 Each IMU's z-axis is aligned with the body segment surface normal, y-axis points upwards,
@@ -30,14 +30,47 @@ and x-axis being perpendicular to the y and z axes following the right-hand rule
 
 <img src="figures/readme_fig/imu_position_and_orientation.png" width="200">
 
-## Links for SSL datasets
+## SSL datasets
 [AMASS](https://amass.is.tue.mpg.de/download.php)
+[1]
 
-[MoVi](https://www.biomotionlab.ca/movi/)
+[MoVi](https://www.biomotionlab.ca/movi/) 
+[2]
 
-## Links for downstream datasets
+## Downstream datasets
 [Downstream dataset 1](https://www.epic.gatech.edu/opensource-biomechanics-camargo-et-al/)
+[3]
 
 [Downstream dataset 2](https://simtk.org/projects/imukinetics)
+[4]
+
+Downstream dataset 3 [5]
 
 ## References
+[1] Mahmood, Naureen, et al. "AMASS: Archive of motion capture as surface shapes."
+*Proceedings of the IEEE/CVF international conference on computer vision*. 2019.
+
+[2] Ghorbani, Saeed, et al. "MoVi: A large multi-purpose human motion and video dataset."
+*Plos One* 16.6 (2021): e0253157.
+
+[3] Camargo, Jonathan, et al. "A comprehensive, open-source dataset of lower limb biomechanics
+in multiple conditions of stairs, ramps, and level-ground ambulation and transitions."
+*Journal of Biomechanics* 119 (2021): 110320.
+
+[4] Tan, Tian, et al. "IMU and smartphone camera fusion for knee adduction and knee flexion
+moment estimation during walking." *IEEE Transactions on Industrial Informatics* 19.2 (2022): 1445-1455.
+
+[5] Sun, Tao, et al. "Real-time ground reaction force and knee extension moment estimation
+during drop landings via modular LSTM modeling and wearable IMUs."
+*IEEE Journal of Biomedical and Health Informatics* (2023).
+
+
+
+
+
+
+
+
+
+
+
---
+++ example_usage/run_example.py
@@ -0,0 +1,417 @@
+import numpy as np
+import h5py
+from torch import nn, Tensor
+from torch.nn import TransformerEncoderLayer, TransformerEncoder
+from torch.nn import functional as F
+import torch
+import json
+import random
+from typing import List
+import copy
+import math
+import pytorch_warmup as warmup
+import matplotlib.pyplot as plt
+import time
+from torch.utils.data import DataLoader, TensorDataset
+from sklearn.metrics import r2_score, mean_squared_error as mse
+from scipy.stats import pearsonr
+from sklearn.preprocessing import StandardScaler
+
+
+CAMARGO_SUB_HEIGHT_WEIGHT = {
+    'AB06': [1.80, 74.8], 'AB07': [1.65, 55.3], 'AB09': [1.63, 63.5], 'AB10': [1.75, 83.9], 'AB11': [1.75, 77.1],
+    'AB12': [1.74, 86.2], 'AB13': [1.73, 59.0], 'AB14': [1.52, 58.4], 'AB15': [1.78, 96.2], 'AB16': [1.65, 55.8],
+    'AB17': [1.68, 61.2], 'AB19': [1.70, 68.0], 'AB21': [1.57, 58.1], 'AB23': [1.80, 76.8], 'AB24': [1.73, 72.6],
+    'AB25': [1.63, 52.2], 'AB27': [1.70, 68.0], 'AB28': [1.69, 62.1], 'AB30': [1.77, 77.0]
+}
+GRAVITY = 9.81
+
+
+# def load_and_process_camargo(self, train_sub_ids: List[str], validate_sub_ids: List[str], test_sub_ids: List[str]):
+#     """
+#     train_sub_ids: a list of subject id for model training
+#     validate_sub_ids: a list of subject id for model validation
+#     test_sub_ids: a list of subject id for model testing
+#     """
+#     self.set_data = {}
+#     with h5py.File('Camargo_levelground.h5', 'r') as hf:
+#         self.data_columns = json.loads(hf.attrs['columns'])
+#         for set_sub_ids, data_name in zip([train_sub_ids, validate_sub_ids, test_sub_ids], ['train', 'vali', 'test']):
+#             current_set_data_list = []
+#             for sub_id in set_sub_ids:
+#                 sub_data = hf[sub_id][:, :, :]
+#                 sub_weight = CAMARGO_SUB_HEIGHT_WEIGHT[sub_id][1] * GRAVITY
+#                 force_col_loc = [self.data_columns.index(x) for x in ['fx', 'fy', 'fz']]
+#                 sub_data[:, force_col_loc] = sub_data[:, force_col_loc] / sub_weight
+#                 current_set_data_list.append(sub_data)
+#             current_set_data = np.concatenate(current_set_data_list, axis=0)
+#             """ [step, feature, time] """
+#             # use rand noise to replace reduced IMUs
+#             rand_noise = np.random.normal(size=(current_set_data.shape[0], 6, current_set_data.shape[2]))
+#             current_set_data = np.concatenate([current_set_data, rand_noise], axis=1)
+#             self.set_data[data_name] = current_set_data
+#         self.data_columns.extend(['rand_noise' + sensor + axis for sensor in ['_Accel_', '_Gyro_'] for axis in ['X', 'Y', 'Z']])
+
+
+class RegressNet(nn.Module):
+    def __init__(self, emb_net, output_dim):
+        super(RegressNet, self).__init__()
+        self.emb_net = emb_net
+        self.emb_output_dim = emb_net.x_dim
+        self.output_dim = output_dim
+        self.linear = nn.Linear(self.emb_output_dim, output_dim)
+
+    def forward(self, x):
+        batch_size, _, seq_len = x[0].shape
+        mod_all = torch.concat(x, dim=1)
+        mod_outputs, _, _ = self.emb_net(mod_all)
+
+        output = mod_outputs.transpose(1, 2)
+        output = self.linear(output)
+        output = output.transpose(1, 2)
+        return output, mod_outputs
+
+
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model, max_len, dropout: float = 0.):
+        super().__init__()
+        self.dropout = nn.Dropout(p=dropout)
+        position = torch.arange(max_len).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2) * (-math.log(max_len * 2) / d_model))
+        pe = torch.zeros(max_len, 1, d_model)
+        pe[:, 0, 0::2] = torch.sin(position * div_term)
+        try:
+            pe[:, 0, 1::2] = torch.cos(position * div_term)
+        except RuntimeError:
+            pe[:, 0, 1::2] = torch.cos(position * div_term)[:, :-1]
+
+        pe = pe.transpose(0, 1)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x: Tensor) -> Tensor:
+        """
+        Args:
+            x: Tensor, shape [batch_size, seq_len, embedding_dim]
+        """
+        x = x + self.pe[:, :x.shape[1], :]
+        return self.dropout(x)
+
+
+class TransformerBase(nn.Module):
+    def __init__(self, imu_to_use, device='cuda'):
+        super().__init__()
+        imu_to_use_mapped, mask_input_imu_index = self.imu_name_mapping(imu_to_use)
+        x_dim = 48
+        patch_len = 8
+        patch_step_len = 8
+        self.patch_len = patch_len
+        self.patch_step_len = patch_step_len
+        self.pad_len = 0
+        self.x_dim = x_dim
+        self.d_model = int(self.x_dim * patch_len)
+        encoder_layers = TransformerEncoderLayer(self.d_model, 48, 512, batch_first=True)
+
+        self.transformer_encoder = TransformerEncoder(encoder_layers, 6)
+        self.device = device
+        # self.mask_patch_num = mask_patch_num
+        self.ssl_mask_emb = nn.Parameter(torch.zeros([1, x_dim * patch_len]).uniform_() - 0.5)
+        self.reduced_imu_mask_emb = nn.Parameter(torch.zeros([48, 128]).uniform_() - 0.5)
+        self.mask_input_imu_index = mask_input_imu_index
+        self.patch_num = int(128 / self.patch_len)
+        self.pos_encoding = PositionalEncoding(x_dim * patch_len, self.patch_num)
+
+    def forward(self, sequence):
+        sequence = self.apply_reduced_imu_set_mask(sequence)
+        sequence_patch = self.divide_into_patches(sequence)
+        sequence_patch, mask_indices = self.apply_ssl_mask(sequence_patch)
+        sequence_patch = self.pos_encoding(sequence_patch)
+        sequence_patch = self.transformer_encoder(sequence_patch)
+
+        output = self.flat_patches(sequence_patch)
+        mask_indices = self.flat_patches(mask_indices)
+        return output, mask_indices, sequence
+
+    def divide_into_patches(self, sequence):
+        sequence = F.pad(sequence, (0, 0, self.pad_len, self.pad_len))
+        sequence = sequence.unfold(2, self.patch_len, self.patch_step_len)
+        sequence = sequence.transpose(1, 2).flatten(start_dim=2)
+        return sequence
+
+    def flat_patches(self, sequence):
+        sequence = sequence.view([*sequence.shape[:2], -1, self.patch_len])
+        sequence = sequence.transpose(1, 2).flatten(start_dim=2)
+        return sequence
+
+    def apply_reduced_imu_set_mask(self, seq):
+        bs, patch_emb, length = seq.shape
+        mask_indices_np = np.full((bs, patch_emb), False)
+        for i_imu in range(8):
+            if i_imu in self.mask_input_imu_index:
+                mask_indices_np[:, i_imu*3:(i_imu+1)*3] = True
+                mask_indices_np[:, i_imu*3+24:(i_imu+1)*3+24] = True
+        mask_indices = torch.from_numpy(mask_indices_np).to(seq.device)
+        mask_indices = mask_indices.unsqueeze(-1).expand_as(seq)
+
+        tensor = torch.mul(seq, ~mask_indices) + torch.mul(self.reduced_imu_mask_emb, mask_indices)
+        return tensor
+
+    @staticmethod
+    def imu_name_mapping(imu_to_use):
+        standard_list = ('CHEST', 'WAIST', 'R_THIGH', 'L_THIGH', 'R_SHANK', 'L_SHANK', 'R_FOOT', 'L_FOOT')
+        imu_to_use_mapped = []
+        mask_input_imu_index = []
+        for i_imu, imu in enumerate(standard_list):
+            if imu in imu_to_use:
+                imu_to_use_mapped.append(imu)
+            else:
+                imu_to_use_mapped.append('rand_masking')
+                mask_input_imu_index.append(i_imu)
+        return imu_to_use_mapped, mask_input_imu_index
+
+
+def load_data(data_file, imu_to_load, outputs, sub_for_fine_tuning, sub_for_testing):
+    data_downstream_dict = {}
+    data_scalar = {'base_scalar': StandardScaler}
+    with h5py.File(data_file, 'r') as hf:
+        data_columns = json.loads(hf.attrs['columns'])
+        data_columns.extend(['rand_noise' + sensor + axis for sensor in ['_Accel_', '_Gyro_'] for axis in ['X', 'Y', 'Z']])
+
+        for set_sub_ids, set_name, norm_method in zip([sub_for_fine_tuning, sub_for_testing], ['tuning', 'test'],
+                                                       ['fit_transform', 'transform']):
+            current_set_data_list = []
+            for sub_id in set_sub_ids:
+                sub_data = hf[sub_id][:, :, :]
+                sub_weight = CAMARGO_SUB_HEIGHT_WEIGHT[sub_id][1] * GRAVITY
+                force_col_loc = [data_columns.index(x) for x in ['fx', 'fy', 'fz']]
+                sub_data[:, force_col_loc] = sub_data[:, force_col_loc] / sub_weight
+                current_set_data_list.append(sub_data)
+            current_set_data = np.concatenate(current_set_data_list, axis=0)
+            """ [step, feature, time] """
+            # use rand noise to replace reduced IMUs
+            rand_noise = np.random.normal(size=(current_set_data.shape[0], 6, current_set_data.shape[2]))
+            current_set_data = np.concatenate([current_set_data, rand_noise], axis=1)
+
+            channel_names = {
+                'acc': [segment + '_Accel_' + axis for segment in imu_to_load for axis in ['X', 'Y', 'Z']],
+                'gyr': [segment + '_Gyro_' + axis for segment in imu_to_load for axis in ['X', 'Y', 'Z']]}
+
+            sampled_data = copy.deepcopy(current_set_data)
+            set_data, data_scalar = preprocess_modality(data_columns, data_scalar, sampled_data, channel_names, norm_method)
+
+            output_data = sampled_data[:, [data_columns.index(x) for x in outputs]]
+            set_data['output'] = normalize_data(data_scalar, output_data, 'output', norm_method, 'by_each_column')
+            set_data['sub_id'] = sampled_data[:, data_columns.index('sub_id'), 0]
+            if 'trial_type_id' in data_columns:
+                set_data['trial_type_id'] = sampled_data[:, data_columns.index('trial_type_id'), 0]
+            else:
+                set_data['trial_type_id'] = np.zeros([sampled_data.shape[0]])
+            data_downstream_dict[set_name] = set_data
+    return data_downstream_dict, data_scalar
+
+
+def normalize_data(data_scalar, data, name, method, scalar_mode, with_mean=False):
+    if method == 'fit_transform':
+        data_scalar[name] = copy.deepcopy(data_scalar['base_scalar'](with_mean=with_mean))
+    assert (scalar_mode in ['by_each_column', 'by_all_columns'])
+
+    input_data = data.copy()
+    size, channel, length = input_data.shape
+    target_shape = [-1, input_data.shape[1]] if scalar_mode == 'by_each_column' else [-1, 1]
+    zero_loc = (input_data == 0.).all(axis=1)
+    for i in range(input_data.shape[1]):
+        input_data[:, i][zero_loc] = np.nan
+    input_data = input_data.transpose(0, 2, 1).reshape(target_shape) if scalar_mode == 'by_each_column' else input_data.reshape(target_shape)
+    scaled_data = getattr(data_scalar[name], method)(input_data)
+    scaled_data = scaled_data.reshape(size, length, channel).transpose(0, 2, 1) if scalar_mode == 'by_each_column' else scaled_data.reshape(size, channel, length)
+    scaled_data[np.isnan(scaled_data)] = 0.
+    return scaled_data
+
+
+def preprocess_modality(data_columns, data_scalar, data_, channel_names, norm_method):
+    processed_data = {}
+    for group_name, cols in channel_names.items():
+        col_loc = [data_columns.index(col) for col in cols]
+        group_data = data_[:, col_loc, :]
+        group_data = normalize_data(data_scalar, group_data, group_name, norm_method, 'by_all_columns')
+        processed_data[group_name] = group_data
+    return processed_data, data_scalar
+
+
+def get_scores(y_true, y_pred, y_fields, lens):
+    scores = []
+    for col, field in enumerate(y_fields):
+        if len(y_true.shape) == 2:
+            r2 = r2_score(y_true[:, col], y_pred[:, col])
+            rmse = np.sqrt(mse(y_true[:, col], y_pred[:, col]))
+            cor_value = pearsonr(y_true[:, col], y_pred[:, col])[0]
+        else:
+            r2, rmse, cor_value = [np.zeros(y_true.shape[0]) for _ in range(3)]
+            for i_step in range(y_true.shape[0]):
+                y_true_one_step = y_true[i_step, col, :lens[i_step]]
+                y_pred_one_step = y_pred[i_step, col, :lens[i_step]]
+                r2[i_step] = r2_score(y_true_one_step, y_pred_one_step)
+                rmse[i_step] = np.sqrt(mse(y_true_one_step, y_pred_one_step))
+                cor_value[i_step] = pearsonr(y_true_one_step, y_pred_one_step)[0]
+        score_one_field = {'field': field, 'r2': r2, 'rmse': rmse, 'cor_value': cor_value}
+        scores.append(score_one_field)
+    return scores
+
+def inverse_normalize_output(y_true, y_pred):
+    y_true = normalize_data(data_scalar, y_true, 'output', 'inverse_transform', 'by_each_column')
+    y_pred = normalize_data(data_scalar, y_pred, 'output', 'inverse_transform', 'by_each_column')
+    return y_true, y_pred
+
+
+def prepare_dl(data_list, batch_size, shuffle, drop_last=False):
+    data_list_torch = [torch.from_numpy(data).float() for data in data_list]
+    ds = TensorDataset(*data_list_torch)
+    dl = DataLoader(ds, batch_size=batch_size, shuffle=shuffle, drop_last=drop_last)
+    return dl
+
+
+def set_dtype_and_model(device, model):
+    if device == 'cuda':
+        dtype = torch.cuda.FloatTensor
+        model.cuda()
+    else:
+        dtype = torch.FloatTensor
+    return dtype, model
+
+
+def model_fine_tuning(linear_protocol, regress_net, data_downstream_dict, show_fig=False, verbose=True):
+    def convert_batch_data(batch_data):
+        xb = [data_.float().type(dtype) for data_ in batch_data[:-2]]
+        yb = batch_data[-2].float().type(dtype)
+        return xb, yb
+
+    def train_batch(regress_net, train_dl, optimizer, loss_fn, i_optimize):
+        regress_net.train()
+        for i_batch, batch_data in enumerate(train_dl):
+            optimizer.zero_grad()
+            xb, yb, lens = convert_batch_data(batch_data)
+            y_pred, _ = regress_net(xb, lens)
+            loss = loss_fn(yb, y_pred)
+            loss.backward()
+            optimizer.step()
+            i_optimize += 1
+            with warmup_scheduler.dampening():
+                scheduler.step()
+        return i_optimize
+
+    def eval_during_training(regress_net, dl, loss_fn, use_batch_num=5):
+        regress_net.eval()
+        loss = []
+        with torch.no_grad():
+            for i_batch, batch_data in enumerate(dl):
+                if i_batch >= use_batch_num:
+                    return np.mean(loss)
+                xb, yb = convert_batch_data(batch_data)
+                y_pred, _ = regress_net(xb)
+                loss.append(loss_fn(yb, y_pred).item())
+        return np.mean(loss)
+
+    def evaluate_after_training(test_dl):
+        regress_net.eval()
+        with torch.no_grad():
+            y_pred_list, y_true_list = [], []
+            for i_batch, batch_data in enumerate(test_dl):
+                xb, yb, lens = convert_batch_data(batch_data)
+                y_true_list.append(yb.detach().cpu())
+                y_pred_batch, mod_outputs_batch = regress_net(xb, lens)
+                y_pred_list.append(y_pred_batch.detach().cpu())
+            y_true = torch.cat(y_true_list).numpy()
+            y_pred = torch.cat(y_pred_list).numpy()
+        return y_true, y_pred
+
+    # test_name = 'LinearProb_' + str(linear_protocol) + ', UseSsl_' + str(use_ssl) + \
+    #             ', ratio_' + str(self.config.da_use_ratio) + self.config.FileNameAppendix
+    # if linear_protocol:
+    #     self.set_regress_net_to_post_ssl_state()
+    train_data, test_data = data_downstream_dict['tuning'], data_downstream_dict['test']
+    train_input_data = [train_data[mod] for mod in ['acc', 'gyr']]
+    train_output_data = train_data['output']
+    # train_step_lens = get_step_len(train_input_data[0])
+    train_dl = prepare_dl([*train_input_data, train_output_data], 64, shuffle=True)
+    test_input_data = [test_data[mod] for mod in ['acc', 'gyr']]
+    test_output_data = test_data['output']
+    # test_step_lens = get_step_len(test_input_data[0])
+    test_dl = prepare_dl([*test_input_data, test_output_data], 64, shuffle=False)
+    dtype, regress_net = set_dtype_and_model('cuda', regress_net)
+
+    if linear_protocol:
+        lr_ = 1e-3
+        param_to_train = regress_net.linear.parameters()
+    else:
+        lr_ = 1e-4
+        param_to_train = regress_net.parameters()
+
+    optimizer = torch.optim.AdamW(param_to_train, lr_)
+    epoch_end_time = time.time()
+    num_of_back_propagation = 300
+
+    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=300)
+    warmup_scheduler = warmup.LinearWarmup(optimizer, warmup_period=int(300/5))
+
+    i_optimize = 0
+    for i_epoch in range(num_of_back_propagation):
+        if verbose:
+            train_loss = eval_during_training(regress_net, train_dl, torch.nn.MSELoss())
+            test_loss = eval_during_training(regress_net, test_dl, torch.nn.MSELoss())
+            if num_of_back_propagation < 2 or i_epoch % int(num_of_back_propagation / 2) == 0 or i_epoch == num_of_back_propagation - 1:
+                print(f'| Regressibility | num_of_back_propagation{i_epoch:3d}/{num_of_back_propagation:3d} |'
+                      f' time: {time.time() - epoch_end_time:5.2f}s |'
+                      f' train loss {train_loss:5.3f} | test loss {test_loss:5.3f}')
+            epoch_end_time = time.time()
+
+            # if i_epoch in [0, int(num_of_back_propagation/2), num_of_back_propagation-1] and self.config.log_with_wandb:
+            #     regress_net.eval()
+            #     with torch.no_grad():
+            #         data_ = list(enumerate(test_dl))[0][1]
+            #         xb_mods = [xb_mod.float().type(dtype) for xb_mod in data_[:-2]]
+            #         outputs, _ = regress_net(xb_mods, None)
+                    # show_reconstructed_signal(data_[-2], outputs, f'num_of_back_propagation {i_epoch}',
+                    #                           fig_group=f"use_ssl {use_ssl}, linear {linear_protocol}",
+                    #                           channel_names=self.output_columns)
+
+        i_optimize = train_batch(regress_net, train_dl, optimizer, torch.nn.MSELoss(), i_optimize)
+
+    if linear_protocol:
+        return regress_net
+
+    y_true, y_pred = evaluate_after_training(test_dl)
+    y_true, y_pred = inverse_normalize_output(y_true, y_pred)
+    # if linear_protocol:
+    #     save_model_and_results(test_name, y_true, y_pred, test_data['sub_id'], self.regress_net)
+    if show_fig:
+        plt.figure()
+        for i_output in range(y_true.shape[1]):
+            plt.title('test set')
+            plt.plot(y_true[:, i_output].ravel(), '-', color='C'+str(i_output), label=outputs[i_output])
+            plt.plot(y_pred[:, i_output].ravel(), '--', color='C'+str(i_output), label=outputs[i_output])
+            plt.legend()
+        plt.show()
+    # if verbose:
+    #     all_scores = get_scores(y_true, y_pred, outputs[i_output], test_step_lens)
+    #     all_scores = [{'subject': 'all', **scores} for scores in all_scores]
+    #     print_table(all_scores)
+
+
+
+if __name__ == "__main__":
+    imu_to_use = ['CHEST', 'R_THIGH']
+    sub_for_fine_tuning = list(CAMARGO_SUB_HEIGHT_WEIGHT.keys())[:-3]
+    sub_for_testing = list(CAMARGO_SUB_HEIGHT_WEIGHT.keys())[-3:]
+    outputs = ['fy']
+    emb_net = TransformerBase(imu_to_use)
+    emb_net.load_state_dict(torch.load('pretrained_model_weights.pth'))
+    regress_net = RegressNet(emb_net, len(outputs))
+    set_data, data_scalar = load_data('Camargo_levelground.h5', imu_to_use, outputs, sub_for_fine_tuning, sub_for_testing)
+    # First train the last linear layer to preserve representation from pre-training
+    regress_net = model_fine_tuning(True, regress_net, set_data)
+    model_fine_tuning(False, regress_net, set_data, True)
+
+
+
+
+
+
